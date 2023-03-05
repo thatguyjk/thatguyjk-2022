5582f824ee9805aee8ef5967d488f56ae56ba2f5
thatguyjk <jonathank.kelly@gmail.com>
2023-03-05 16:38:33 -0500

    updating project configs

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
