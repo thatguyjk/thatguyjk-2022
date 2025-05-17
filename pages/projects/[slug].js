9b6adabacc8b2a37f5417fdf9fc9c3685811b1a1
Jonathan Kelly <jonathan.kelly@instrument.com>
2025-05-17 11:49:40 -0400

    fixing next image attributes

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,122 +1,166 @@
-import { useRouter } from 'next/router';
-import Head from 'next/head';
-import Image from 'next/image';
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
 import { getAllProjects } from "../../lib/api";
-import { BLOCKS, INLINES, MARKS } from '@contentful/rich-text-types';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, INLINES, MARKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 // import { gsap } from "gsap";
 
-export default function Project({createdAt, projectYear, description, techStack, title, url, projectImages}) {
-    const route = useRouter();
+export default function Project({
+  createdAt,
+  projectYear,
+  description,
+  techStack,
+  title,
+  url,
+  projectImages,
+}) {
+  const route = useRouter();
 
-    const Bold = ({children}) => <b>{children}</b>;
-    const Text = ({children}) => <p>{children}</p>;
-    const Italic = ({children}) => <em>{children}</em>
-    const Code = ({children}) => <code>{children}</code>
+  const Bold = ({ children }) => <b>{children}</b>;
+  const Text = ({ children }) => <p>{children}</p>;
+  const Italic = ({ children }) => <em>{children}</em>;
+  const Code = ({ children }) => <code>{children}</code>;
 
-    const options = {
-      renderMark: {
-        [MARKS.BOLD]: text => <Bold classes="">{ text }</Bold>,
-        [MARKS.ITALIC]: text => <Italic classes="">{ text }</Italic>,
-        [MARKS.CODE]: text => <Code classes="">{ text }</Code>
-      },
-      renderNode: {
-        [BLOCKS.PARAGRAPH]: (node, children) => <Text classes="text-2xl mb-4">{ children }</Text>,
-        [INLINES.HYPERLINK]: ({data}, children) => <a className="text-red" href={data.uri} target="_blank" rel="noopener noreferrer">{children}</a>
-      }
-    }
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: (text) => <Bold classes=''>{text}</Bold>,
+      [MARKS.ITALIC]: (text) => <Italic classes=''>{text}</Italic>,
+      [MARKS.CODE]: (text) => <Code classes=''>{text}</Code>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <Text classes='text-2xl mb-4'>{children}</Text>
+      ),
+      [INLINES.HYPERLINK]: ({ data }, children) => (
+        <a
+          className='text-red'
+          href={data.uri}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {children}
+        </a>
+      ),
+    },
+  };
 
-    return (
-      <>
-        <Head key='title'>
-          <title>ThatGuyJK - Projects - {title ? title : null}</title>
-        </Head>
-        <article className='py-16 min-h-screen'>
-          <section className='flex flex-row flex-wrap items-baseline justify-between mb-6'>
-            <h1 className='font-roboto text-4xl md:text-5xl'>{title}</h1>
-            <button type='button' className='font-nunito' onClick={() => route.push('/projects') }>
-              Go Back
-            </button>
-          </section>
+  return (
+    <>
+      <Head key='title'>
+        <title>ThatGuyJK - Projects - {title ? title : null}</title>
+      </Head>
+      <article className='py-16 min-h-screen'>
+        <section className='flex flex-row flex-wrap items-baseline justify-between mb-6'>
+          <h1 className='font-roboto text-4xl md:text-5xl'>{title}</h1>
+          <button
+            type='button'
+            className='font-nunito'
+            onClick={() => route.push("/projects")}
+          >
+            Go Back
+          </button>
+        </section>
 
-          <section className='grid gap-3 mb-8 md:grid-cols-2'>
-            <div className='font-nunito'>
+        <section className='grid gap-3 mb-8 md:grid-cols-2'>
+          <div className='font-nunito'>
             {documentToReactComponents(description, options)}
+          </div>
+
+          <div className='grid auto-rows-auto gap-4 font-nunito md:border-l-2 md:border-l-black md:pl-3'>
+            <div className='break-all'>
+              <h6 className='underline underline-offset-4 text-xl font-bold'>
+                Developed At
+              </h6>
+              <p className='text-lg'>
+                {createdAt ? createdAt : null}&nbsp;
+                {projectYear ? "(" + projectYear + ")" : null}
+              </p>
             </div>
 
-            <div className='grid auto-rows-auto gap-4 font-nunito md:border-l-2 md:border-l-black md:pl-3'>
-              <div className='break-all'>
-                <h6 className='underline underline-offset-4 text-xl font-bold'>Developed At</h6>
-                <p className='text-lg'>{createdAt ? createdAt : null}&nbsp;{ projectYear ? '('+projectYear+')' : null}</p>
-              </div>
-
-              <div className='break-all'>
-                <h6 className='underline underline-offset-4 text-xl font-bold'>Technology Used</h6>
-                <ul>
-                  {techStack && techStack.map((tech, id) => {
-                    return <li key={id} className='text-sm font-semibold bg-carbon p-1 rounded-sm inline-block mr-2 mt-1'>{tech ? tech : null}</li>;
+            <div className='break-all'>
+              <h6 className='underline underline-offset-4 text-xl font-bold'>
+                Technology Used
+              </h6>
+              <ul>
+                {techStack &&
+                  techStack.map((tech, id) => {
+                    return (
+                      <li
+                        key={id}
+                        className='text-sm font-semibold bg-carbon p-1 rounded-sm inline-block mr-2 mt-1'
+                      >
+                        {tech ? tech : null}
+                      </li>
+                    );
                   })}
-                </ul>
-              </div>
-
-              <div className='break-all'>
-                <h6 className='underline underline-offset-4 text-xl font-bold'>Project URL</h6>
-                <a className="hover:text-red" href={url && url.includes("[offline]") ? '#' : url} target='_blank' rel='noreferrer'>
-                  {url ? url : 'Not Available'}
-                </a>
-              </div>
+              </ul>
             </div>
-          </section>
-          
-          <section className='columns-1 md:columns-2 items-start'>
-          {projectImages && projectImages.map((prjImg, imgId) => {
+
+            <div className='break-all'>
+              <h6 className='underline underline-offset-4 text-xl font-bold'>
+                Project URL
+              </h6>
+              <a
+                className='hover:text-red'
+                href={url && url.includes("[offline]") ? "#" : url}
+                target='_blank'
+                rel='noreferrer'
+              >
+                {url ? url : "Not Available"}
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <section className='columns-1 md:columns-2 items-start'>
+          {projectImages &&
+            projectImages.map((prjImg, imgId) => {
               return (
-                <figure key={imgId} className="mb-4">
+                <figure key={imgId} className='mb-4'>
                   <Image
-                    src={'/' + prjImg.fields?.file.fileName}
+                    src={"/" + prjImg.fields?.file.fileName}
                     alt={prjImg.fields?.title}
-                    layout='intrinsic'
                     width={prjImg.fields?.file?.details?.image?.width}
                     height={prjImg.fields?.file?.details?.image?.height}
-                    data-type="prjImage"
+                    data-type='prjImage'
                     className='rounded'
                   />
                 </figure>
               );
             })}
-          </section>          
-        </article>
-
-      </>
-    );
+        </section>
+      </article>
+    </>
+  );
 }
 
 export async function getStaticPaths() {
-    const allProjects = await getAllProjects();
+  const allProjects = await getAllProjects();
 
-    allProjects.items.forEach((project) => {
-      delete project["sys"];
-      delete project["metadata"];
-    });
+  allProjects.items.forEach((project) => {
+    delete project["sys"];
+    delete project["metadata"];
+  });
 
-    const paths = allProjects.items.map((prj) => ({
-        params: { slug: prj.fields.slug }
-    }));
+  const paths = allProjects.items.map((prj) => ({
+    params: { slug: prj.fields.slug },
+  }));
 
-    return { 
-        paths,
-        fallback: false,
-    }
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   const allProjects = await getAllProjects();
 
   const portfolioItem = allProjects.items.filter((project) => {
-    delete project['metadata'];
-    delete project['sys'];
+    delete project["metadata"];
+    delete project["sys"];
 
-    return project['fields']['slug'] === params.slug;
+    return project["fields"]["slug"] === params.slug;
   });
 
   return {
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,52 +1,58 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { getAllProjects } from '../../lib/api';
+import Head from "next/head";
+import Link from "next/link";
+import { getAllProjects } from "../../lib/api";
 
-function Projects({projects}) {
-    return (
-      <>
-        <Head>
-          <title>ThatGuyJK - Projects</title>
-        </Head>
-        <article className='py-16 min-h-screen'>
-          <h5 className='mb-8 font-nunito text-4xl'>Featured Projects</h5>
-          <ul className='grid grid-cols-1 md:grid-cols-2 justify-items-center items-center'>
-            {projects.map((project, index) => {
-                return (
-                  <Link href={`/projects/${project.fields.slug}`} key={index}>
-                    <li className='relative transition-color text-xl text-center md:text-5xl px-3 aspect-[4/3] h-32 md:h-96 w-full flex justify-center items-center cursor-pointer hover:text-white'>
-                      <span
-                        className='transition-opacity pointer-events-auto w-full h-full absolute inset-0 opacity-0 hover:opacity-100'
-                        style={{ backgroundColor: project.fields.primaryColor }}
-                      ></span>
-                      <span className='z-10 pointer-events-none'>
-                        {project.fields.title}
-                      </span>
-                    </li>
-                  </Link>
-                );
-              })}
-          </ul>
-        </article>
-      </>
-    );
+function Projects({ projects }) {
+  return (
+    <>
+      <Head>
+        <title>ThatGuyJK - Projects</title>
+      </Head>
+      <article className='py-16 min-h-screen'>
+        <h5 className='mb-8 font-nunito text-4xl'>Featured Projects</h5>
+        <ul className='grid grid-cols-1 md:grid-cols-2 justify-items-center items-center'>
+          {projects.map((project, index) => {
+            return (
+              <Link
+                href={`/projects/${project.fields.slug}`}
+                key={index}
+                className='w-full'
+              >
+                <li className='relative transition-color text-xl text-center md:text-5xl px-3 aspect-[4/3] h-32 md:h-96 w-full flex justify-center items-center cursor-pointer hover:text-white'>
+                  <span
+                    className='transition-opacity pointer-events-auto w-full h-full absolute inset-0 opacity-0 hover:opacity-100'
+                    style={{ backgroundColor: project.fields.primaryColor }}
+                  ></span>
+                  <span className='z-10 pointer-events-none'>
+                    {project.fields.title}
+                  </span>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </article>
+    </>
+  );
 }
 
 export async function getStaticProps() {
-    const allProjects = await getAllProjects();
-    
-    allProjects.items.forEach(project => {
-        delete project['sys'];
-        delete project['metadata'];
-    });
+  const allProjects = await getAllProjects();
 
-    allProjects.items.sort((a, b) => a.fields.projectOrder - b.fields.projectOrder);
+  allProjects.items.forEach((project) => {
+    delete project["sys"];
+    delete project["metadata"];
+  });
 
-    return {
-        props: {
-            projects: allProjects.items,
-        },
-    }
+  allProjects.items.sort(
+    (a, b) => a.fields.projectOrder - b.fields.projectOrder
+  );
+
+  return {
+    props: {
+      projects: allProjects.items,
+    },
+  };
 }
 
 export default Projects;
